41e27d4928eca7e668683eae0f6c281045de92de
IrvanRizkyAriansyah <irvanrizky58@gmail.com>
2024-07-05 06:08:10 +0700

    vercel

--- config/config.js
+++ config/config.js
@@ -16,5 +16,18 @@ module.exports = {
     },
   },
   test: {},
-  production: {},
+  production: {
+    username: process.env.EXPRESS_DB_USERNAME,
+    password: process.env.EXPRESS_DB_PASSWORD,
+    database: process.env.EXPRESS_DB_NAME,
+    host: process.env.EXPRESS_DB_HOST,
+    dialect: "mysql",
+    port: process.env.EXPRESS_DB_PORT,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
 };
